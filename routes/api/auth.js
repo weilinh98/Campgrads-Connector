914b8102eea4a17d13ec48edb458bc0053656a9e
weilinh98 <weilinhong007@gmail.com>
2020-03-18 12:20:13 -0700

    add auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../../models/User");
+const authMiddleware = require("../../middleware/auth");
 //@route GET api/auth
 //@desc Test route
 //@access Public
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const id = req.user.id;
+    const user = await User.findById(id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
